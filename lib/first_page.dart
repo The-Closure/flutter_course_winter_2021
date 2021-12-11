536ba1ca9518de44a068a0a6f286f66ecac76e7e
anasa <anasa@techno-visions.net>
2021-12-11 19:54:17 +0200

    init point

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -1,50 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_course_winter_2021/data_service.dart';
-import 'package:flutter_course_winter_2021/second_page.dart';
 
-class FirstPage extends StatelessWidget {
-  const FirstPage({Key? key}) : super(key: key);
+class FirstPage extends StatefulWidget {
+  FirstPage({Key? key}) : super(key: key);
 
+  @override
+  _FirstPageState createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage> {
+  int i = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          AnimatedAlign(
-            alignment: Alignment.topLeft,
-            curve: Curves.easeInOutBack,
-            duration: Duration(seconds: 5),
-            child: Hero(
-              tag: 'AnCon',
-              child: Container(
-                height: 100,
-                width: 100,
-                color: Colors.indigo,
-              ),
-            ),
-          ),
-          AnimatedContainer(
-              duration: Duration(seconds: 5),
-              color: Colors.red,
-              width: 200,
-              height: 200,
-              alignment: Alignment.center,
-              child: FutureBuilder(
-                initialData: 0,
-                builder: (ctxt, snapData) => Text(
-                  snapData.data.toString(),
-                  style: TextStyle(fontSize: 32, color: Colors.white),
-                ),
-                future: DataService().getValue(),
-              )),
-        ],
+      body: Center(
+        child: Text(
+          '$i',
+          style: TextStyle(fontSize: 32, color: Colors.indigo),
+        ),
       ),
-      floatingActionButton: FloatingActionButton(onPressed: () {
-        Navigator.of(context)
-            .push(MaterialPageRoute(builder: (_) => SecondPage()));
-      }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            i++;
+          });
+        },
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
--- lib/second_page.dart
+++ lib/second_page.dart
@@ -1,46 +1,121 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_course_winter_2021/data_service.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_course_winter_2021/first_page.dart';
 
-class SecondPage extends StatelessWidget {
+class SecondPage extends StatefulWidget {
   const SecondPage({Key? key}) : super(key: key);
+
+  @override
+  _SecondPageState createState() => _SecondPageState();
+}
+
+class _SecondPageState extends State<SecondPage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  GlobalKey<FormState> formKey = GlobalKey();
+  GlobalKey<FormFieldState> fieldKey = GlobalKey();
+
+  bool? submitCheck() {
+    bool? state = formKey.currentState?.validate();
+    return state;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: Container(
-        padding: EdgeInsets.all(100),
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            AnimatedAlign(
-              alignment: Alignment.topLeft,
-              curve: Curves.easeInOutBack,
-              duration: Duration(seconds: 5),
-              child: Hero(
-                tag: 'AnCon',
+        padding: EdgeInsets.all(16),
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                controller: emailController,
+                autofocus: true,
+                cursorColor: Colors.blue,
+                keyboardAppearance: Brightness.dark,
+                // maxLength: 3,
+                // maxLengthEnforcement: MaxLengthEnforcement.enforced,
+                validator: (input) {
+                  if (input != null) {
+                    bool emailValid = RegExp(
+                            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                        .hasMatch(input);
+                    return emailValid ? null : 'check your email address';
+                  } else
+                    return 'enter email address';
+                },
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  icon: Icon(Icons.mail),
+
+                  focusColor: Colors.indigo,
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black)),
+                  // fillColor: Colors.red,
+                  // filled: true,
+                  enabled: true,
+                  disabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(16),
+                    ),
+                    borderSide: BorderSide(
+                        color: Colors.indigo,
+                        style: BorderStyle.solid,
+                        width: 2),
+                  ),
+                  // suffixText: '@gmail.com',
+                  contentPadding: EdgeInsets.all(4),
+                  border: OutlineInputBorder(),
+                  hintText: 'example@email.com',
+                  labelText: 'email address',
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              TextFormField(
+                controller: passwordController,
+                validator: (input) {
+                  if (input != null) {
+                    bool emailValid =
+                        RegExp(r"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$")
+                            .hasMatch(input);
+                    return emailValid ? null : 'weak password';
+                  } else
+                    return 'enter password';
+                },
+                obscureText: true,
+                obscuringCharacter: '*',
+                decoration: InputDecoration(
+                  icon: Icon(Icons.lock),
+                  border: OutlineInputBorder(),
+                  hintText: 'P@ssw0rd',
+                  labelText: 'password',
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  if (submitCheck() ?? false) {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (_) => FirstPage(),
+                      ),
+                    );
+                  }
+                },
                 child: Container(
-                  height: 100,
-                  width: 400,
-                  color: Colors.indigo,
+                  child: Text('singin'),
+                  alignment: Alignment.center,
                 ),
-              ),
-            ),
-            AnimatedContainer(
-              duration: Duration(seconds: 5),
-              color: Colors.red,
-              width: 200,
-              height: 200,
-              child: StreamBuilder(
-                builder: (ctxt, snapData) => Text(
-                  snapData.data.toString(),
-                  style: TextStyle(fontSize: 32, color: Colors.white),
-                ),
-                stream: DataService().getStream(),
-                initialData: 0,
-              ),
-            ),
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
