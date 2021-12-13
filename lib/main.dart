f62985fb55c9e1c280e0dff6fac9038d7c891370
anasa <anasa@techno-visions.net>
2021-12-13 19:55:16 +0200

    init point

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SecondPage(),
+      routes: {
+        'dismissPage': (_) => SecondPage(),
+        'GesPage': (_) => FirstPage()
+      },
+      initialRoute: 'GesPage',
     );
   }
 }
--- lib/second_page.dart
+++ lib/second_page.dart
@@ -10,113 +10,84 @@ class SecondPage extends StatefulWidget {
 }
 
 class _SecondPageState extends State<SecondPage> {
-  TextEditingController emailController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
-  GlobalKey<FormState> formKey = GlobalKey();
-  GlobalKey<FormFieldState> fieldKey = GlobalKey();
+  bool dismissDecision = true;
 
-  bool? submitCheck() {
-    bool? state = formKey.currentState?.validate();
-    return state;
-  }
-
+  List<String> data = [
+    'flutter',
+    'dart',
+    'bloc',
+    'ui',
+    'firebase',
+    'github',
+    'gitlab'
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(
-        padding: EdgeInsets.all(16),
-        child: Form(
-          key: formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextFormField(
-                controller: emailController,
-                autofocus: true,
-                cursorColor: Colors.blue,
-                keyboardAppearance: Brightness.dark,
-                // maxLength: 3,
-                // maxLengthEnforcement: MaxLengthEnforcement.enforced,
-                validator: (input) {
-                  if (input != null) {
-                    bool emailValid = RegExp(
-                            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-                        .hasMatch(input);
-                    return emailValid ? null : 'check your email address';
-                  } else
-                    return 'enter email address';
-                },
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  icon: Icon(Icons.mail),
-
-                  focusColor: Colors.indigo,
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black)),
-                  // fillColor: Colors.red,
-                  // filled: true,
-                  enabled: true,
-                  disabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(16),
-                    ),
-                    borderSide: BorderSide(
-                        color: Colors.indigo,
-                        style: BorderStyle.solid,
-                        width: 2),
-                  ),
-                  // suffixText: '@gmail.com',
-                  contentPadding: EdgeInsets.all(4),
-                  border: OutlineInputBorder(),
-                  hintText: 'example@email.com',
-                  labelText: 'email address',
-                ),
-              ),
-              SizedBox(
-                height: 16,
-              ),
-              TextFormField(
-                controller: passwordController,
-                validator: (input) {
-                  if (input != null) {
-                    bool emailValid =
-                        RegExp(r"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$")
-                            .hasMatch(input);
-                    return emailValid ? null : 'weak password';
-                  } else
-                    return 'enter password';
-                },
-                obscureText: true,
-                obscuringCharacter: '*',
-                decoration: InputDecoration(
-                  icon: Icon(Icons.lock),
-                  border: OutlineInputBorder(),
-                  hintText: 'P@ssw0rd',
-                  labelText: 'password',
-                ),
-              ),
-              SizedBox(
-                height: 16,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  if (submitCheck() ?? false) {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (_) => FirstPage(),
-                      ),
-                    );
-                  }
-                },
-                child: Container(
-                  child: Text('singin'),
-                  alignment: Alignment.center,
-                ),
-              )
-            ],
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.preview),
+          onPressed: () {
+            Navigator.of(context).pop('you backed from dimissPage');
+          },
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: data.length,
+        itemBuilder: (ctxt, index) => Dismissible(
+          confirmDismiss: (direction) {
+            return Future.delayed(
+                Duration(milliseconds: 1), () => dismissDecision);
+          },
+          onDismissed: (direction) {
+            if (direction == DismissDirection.startToEnd) {
+              print('deleteing item with index : $index');
+              data.removeAt(index);
+            } else if (direction == DismissDirection.endToStart) {
+              print('archiving item $index');
+              data.removeAt(index);
+            }
+          },
+          secondaryBackground: Container(
+            padding: EdgeInsets.all(8),
+            alignment: Alignment.centerRight,
+            color: Colors.green,
+            child: Icon(
+              Icons.note,
+              size: 32,
+              color: Colors.white,
+            ),
+          ),
+          background: Container(
+            padding: EdgeInsets.all(8),
+            alignment: Alignment.centerLeft,
+            color: Colors.red,
+            child: Icon(
+              Icons.delete,
+              size: 32,
+              color: Colors.white,
+            ),
+          ),
+          key: UniqueKey(),
+          child: Container(
+            margin: EdgeInsets.all(16),
+            color: Colors.indigo,
+            alignment: Alignment.center,
+            width: double.infinity,
+            height: 75,
+            child: Text(
+              '${data[index]}',
+              style: TextStyle(fontSize: 32, color: Colors.white),
+            ),
           ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            dismissDecision = !dismissDecision;
+          });
+        },
       ),
     );
   }
