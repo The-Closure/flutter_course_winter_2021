3bfcbbacf7fe8cba84ed7389678a62583972287d
anasa <anasa@techno-visions.net>
2022-03-18 15:26:04 +0200

    firebase-notifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_course_winter_2021/bloc/auth_bloc.dart';
 import 'package:flutter_course_winter_2021/ui/home.dart';
 
-void main() async {
-  WidgetsFlutterBinding();
+Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  print('Got a message whilst in the foreground!');
+  print('Message data: ${message.data}');
 
+  if (message.notification != null) {
+    print('Message also contained a notification: ${message.notification}');
+  }
+}
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // qrCode.make();
+
+  FirebaseApp app = await Firebase.initializeApp();
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+  FirebaseMessaging.onMessage.listen(firebaseMessagingBackgroundHandler);
+  // FirebaseApp defaultApp = Firebase.app();
   runApp(MyApp());
 }
 
