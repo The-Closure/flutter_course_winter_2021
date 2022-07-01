10593b8923b413951eebacdd5958777bec2459f5
anasa <anasa@techno-visions.net>
2022-07-01 20:21:30 +0300

    add comment

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
+  //example comment
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
